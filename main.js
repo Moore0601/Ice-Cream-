107ba8f0bfb443ed26f4c83a8553d9183ae64aef
moore0601 <muhammedoyebanji@gmail.co>
2025-03-10 16:54:16 +0000

    update

--- main.js
+++ main.js
@@ -41,3 +41,33 @@ ScrollReveal().reveal(".header__content .socials",{
     ...scrollRevealOption,
     delay: 2000,
 });
+
+ScrollReveal().reveal(".popular__card",{
+    ...scrollRevealOption,
+    interval: 500,
+});
+
+ScrollReveal().reveal(".discover__card img",{
+    ...scrollRevealOption,
+    origin: "left",
+});
+ScrollReveal().reveal(".discover__card:nth-child(2) img",{
+    ...scrollRevealOption,
+    origin: "right",
+});
+ScrollReveal().reveal(".discover__card__content h4",{
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".discover__card__content .section__description",{
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".discover__card__content h3",{
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".discover__card__btn",{
+    ...scrollRevealOption,
+    delay: 2000,
+});
